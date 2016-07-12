6f190cc366f53c093e4b91ac02fa1f4a11387b81
Individumm <me@someaddress.de>
2016-07-12 21:21:12 +0200

    Fix build

--- spec/example.spec.js
+++ spec/example.spec.js
@@ -15,14 +15,14 @@ var objectUnderTest = {
 
 describe("simple object using 'complicated' lib", function () {
     var mockRecorder = require('../index.js');
-    var complicatedLib = mockRecorder.wrapper('replay', 'my-complicated-lib-which-is-not-part-of-this-test', function () {
+    var complicatedLib = mockRecorder.wrapper('record', 'my-complicated-lib-which-is-not-part-of-this-test', function () {
         return require('../complicatedLib.js');
     });
 
     it("should return 30 on a()", function () {
         expect(objectUnderTest.a(complicatedLib)).toBe(30);
     });
-    
+
     it("should return 100 on b()", function () {
         expect(objectUnderTest.b(complicatedLib)).toBe(100);
     });
